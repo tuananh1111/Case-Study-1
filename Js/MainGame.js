cc3c53d65ff455c91224ffff16b68acfe14c2a59
tuananh1111 <nguyentuananhchuot0612@gmail.com>
2020-09-04 09:29:46 +0700

    update

--- Js/MainGame.js
+++ Js/MainGame.js
@@ -6,9 +6,9 @@ let myBackground;
 
 
 function startGame(){
-    myGamePiece= new Component(80,80,"icon.png",50,330,"image");
+    myGamePiece= new Component(80,80,"img/icon.png",50,330,"image");
     myScore=new Component("200px","150px", "white", 650,100,"text");
-    myBackground= new Component(1000,750, "background.png", 0,0,"background");
+    myBackground= new Component(1000,750, "img/background.png", 0,0,"background");
     myGameArea.start();
 }
 function updateGameArea() {
